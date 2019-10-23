3031f5b9a94d6df32fcfbb3bfbd78d07dacca22d
Srishti Singh <srishtisingh20014@gmail.com>
2019-10-24 01:15:28 +0530

    Added new codes in amstrong.py and even or odd in list.py
    Instead of a pre-defined list, added the code for user-defined list
    
    and
     in amstrong number code, added three condition for positive , zero and negative numbers

--- Python/armstrong.py
+++ Python/armstrong.py
@@ -1,11 +1,27 @@
 num = int(input("Enter a number: "))
-sum = 0
-temp = num
-while temp > 0:
-   digit = temp % 10
-   sum += digit ** 3
-   temp //= 10
-if num == sum:
-   print(num,"is an Armstrong number")
+
+if(num>0):
+    sum = 0
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** 3
+        temp //= 10
+        if num == sum:
+            print(num,"is an Armstrong number")
+        else:
+            print(num,"is not an Armstrong number")
+if(num=0):
+    print("Zero is not an amstrong number")
 else:
-   print(num,"is not an Armstrong number")
+    num=0-num
+    sum = 0
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** 3
+        temp //= 10
+        if num == sum:
+            print(num,"is an Armstrong number")
+        else:
+            print(num,"is not an Armstrong number")
